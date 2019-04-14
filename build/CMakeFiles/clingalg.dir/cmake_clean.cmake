deff742482d35c48fe891efd0d2fb8c0c7ad6fbe
Mikel Losada <mikel.losada@gmail.com>
2019-04-14 19:23:28 +0200

    Deleted files that where moved somewhere else

---
+++ build/CMakeFiles/clingalg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clingalg.dir/test_all.c.o"
+  "clingalg.pdb"
+  "clingalg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clingalg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
